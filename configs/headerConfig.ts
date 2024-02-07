d053b95b345231dac8308874bee81227691cd861
Anton Keller <ToniWebDev@mail.ru>
2024-02-07 16:48:03 +0300

    create config header

---
+++ configs/headerConfig.ts
@@ -0,0 +1,23 @@
+import {Tlink} from "~/utils/types";
+
+export const navLinks: Array<Tlink> = [
+  {
+    id: 1,
+    title: "Услуги",
+    description: "описание",
+    url: "/services",
+  },
+  // {
+  //   id: 2,
+  //   title: "Опыт",
+  //   description: "наши клиенты",
+  //   url: "/experience",
+  // },
+  {
+    id: 3,
+    title: "Достижения",
+    description: "по периодам",
+    url: "/rating",
+  },
+];
+
