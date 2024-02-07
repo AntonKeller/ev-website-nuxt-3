ecdf3fb5c5de4787ae21929c2d44b011364c50f2
Anton Keller <ToniWebDev@mail.ru>
2024-02-07 16:45:19 +0300

    create main page

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="scroll-hidden w-full bg-gray-100">
+    <!--   MAIN BANNER SECTION   -->
+    <bannerSection/>
+    <!--   CUSTOMERS SECTION   -->
+    <experienceSection/>
+    <!--   MANAGERS SECTION   -->
+    <managersSection />
+    <!--   FOOTER   -->
+    <evFooter/>
+  </div>
+</template>
+
+<script>
+
+import bannerSection from "@/components/sections/ev-banner-section/ev-banner";
+import experienceSection from "@/components/sections/ev-customer-section/ev-experience-section";
+import managersSection from "@/components/sections/ev-manager-section/ev-managers";
+import evFooter from "@/components/widgets/ev-footer";
+
+export default {
+  name: 'IndexPage',
+  components: {
+    bannerSection,
+    experienceSection,
+    managersSection,
+    evFooter,
+  },
+  data() {
+    return ({})
+  }
+}
+
+</script>
+
+<style scoped>
+.scroll-hidden::-webkit-scrollbar {
+  width: 0;
+}
+</style>
