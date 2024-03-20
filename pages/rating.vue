c3a37bf985a592b4e4d448c58af221b2f311040c
Anton Keller <ToniWebDev@mail.ru>
2024-03-20 11:29:07 +0300

    change rating styles

--- pages/rating.vue
+++ pages/rating.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="rating-container">
 
-<!--        <div>-->
-<!--          <div-->
-<!--              v-for="title of  getTitles()"-->
-<!--              :key="title.key"-->
-<!--          >-->
-<!--            {{ title.text }}-->
-<!--          </div>-->
-<!--        </div>-->
-<!--        {{getTitles()}}-->
+    <!--        <div>-->
+    <!--          <div-->
+    <!--              v-for="title of  getTitles()"-->
+    <!--              :key="title.key"-->
+    <!--          >-->
+    <!--            {{ title.text }}-->
+    <!--          </div>-->
+    <!--        </div>-->
+    <!--        {{getTitles()}}-->
 
     <div class="z-10 flex flex-col gap-y-4">
 
       <div class="px-6 flex justify-center items-center gap-x-8">
-        <i @click="prevYear" :class="{'text-sky-300/20': inProcess || activeYear <= 2011}"
+        <i @click="prevYear" :class="{'text-sm text-sky-300/30': inProcess || activeYear <= 2011}"
            class="select-none cursor-pointer text-sky-300 ml-4 pi pi-caret-left h-fit"></i>
         <div class="text-sky-300">{{ activeYear }} г</div>
-        <i @click="nextYear" :class="{'text-sky-300/20': inProcess || activeYear >= 2021}"
+        <i @click="nextYear" :class="{'text-sm text-sky-300/30': inProcess || activeYear >= 2021}"
            class="select-none cursor-pointer text-sky-300 ml-4 pi pi-caret-right h-fit"></i>
       </div>
 
@@ -27,12 +27,13 @@
             class="py-1.5 px-4 lg:py-1.5 lg:px-2.5 bg-sky-950 gap-y-1 rounded-xl lg:rounded-full flex flex-col lg:flex-row lg:items-center lg:justify-end"
         >
           <div class="text-sm text-gray-300 font-medium text-center lg:text-right">{{ rating.title }}</div>
-          <div class="relative ml-2 md:min-w-[240px] lg:max-w-[360px] bg-sky-900 rounded-md lg:rounded-full overflow-hidden">
-            <div class="w-full h-fit bg-sky-700 border border-sky-800 shadow-lg rounded-md lg:rounded-full"
-                 :style="{width: rating.value  + '%'}">
+          <div
+              class="relative ml-2 md:min-w-[240px] lg:max-w-[360px] bg-sky-900 rounded-md lg:rounded-full overflow-hidden">
+            <div class="w-full h-fit bg-gradient-to-r from-blue-800 to-sky-700 rounded-md lg:rounded-full"
+                 :style="{width: rating.value * 2 + '%'}">
               <div class="opacity-0 text-xs">-1</div>
             </div>
-            <div class="absolute right-0 top-0 mt-[2.36px] mr-3 text-xs text-sky-300 font-medium">
+            <div class="absolute right-0 top-0 mt-[1px] mr-3 text-xs text-sky-300 font-medium">
               {{ rating.value }} шт
             </div>
           </div>
@@ -41,15 +42,32 @@
 
     </div>
 
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">Преимущества Everest Consulting</div>
     <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">
-      Наши специалисты успешно осуществили и завершили проекты для многих (более 50) компаний из рейтинга «Крупнейшие компании России» (рейтинг Эксперт-400).
+      Наша цель – оказание полного комплекса консультационных услуг в области оценки и консалтинга (финансового,
+      управленческого, налогового и строительного) специалистами, обладающими международными квалификациями и
+      значительным опытом выполнения подобных проектов как в России, так и за рубежом.
     </div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">...</div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">...</div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">...</div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">...</div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">...</div>
+    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">
+      Наши специалисты успешно осуществили и завершили проекты для многих (более 50) компаний из рейтинга «Крупнейшие
+      компании России» (рейтинг Эксперт-400).
+    </div>
+    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">
+      Детальное понимание требований к оценочным и консалтинговым продуктам со стороны аудиторских компаний,
+      коммерческих и инвестиционных банков, страховых компаний.
+    </div>
+    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">
+      Эффективное планирование задач в рамках проектов благодаря значительному опыту сотрудников команды.
+    </div>
+    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">
+      Возможность выполнения проектов в сжатые сроки благодаря существующим наработкам и базе знаний.
+    </div>
+    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">
+      Оптимальное соотношение «цена-качество».
+    </div>
+    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">
+      Возможность привлечения к выполнению проектов специалистов, обладающих международными квалификациями и опытом в
+      различных областях знаний.
+    </div>
     <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">...</div>
     <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">...</div>
     <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">...</div>
@@ -128,7 +146,7 @@ export default {
         this.outNum(
             e.value,
             eI,
-            Math.floor(200 + Math.random() * 800)
+            Math.floor(600 + Math.random() * 200)
         );
       });
     },
