c0a43182f029c6a4559b43804b6739916c467f50
Anton Keller <ToniWebDev@mail.ru>
2024-03-20 14:38:24 +0300

    change rating styles

--- pages/rating.vue
+++ pages/rating.vue
@@ -1,81 +1,76 @@
 <template>
-  <div class="rating-container">
+  <div>
+    <div class="rating-container">
 
-    <!--        <div>-->
-    <!--          <div-->
-    <!--              v-for="title of  getTitles()"-->
-    <!--              :key="title.key"-->
-    <!--          >-->
-    <!--            {{ title.text }}-->
-    <!--          </div>-->
-    <!--        </div>-->
-    <!--        {{getTitles()}}-->
-
-    <div class="z-10 flex flex-col gap-y-4">
-
-      <div class="px-6 flex justify-center items-center gap-x-8">
-        <i @click="prevYear" :class="{'text-sm text-sky-300/30': inProcess || activeYear <= 2011}"
-           class="select-none cursor-pointer text-sky-300 ml-4 pi pi-caret-left h-fit"></i>
-        <div class="text-sky-300">{{ activeYear }} г</div>
-        <i @click="nextYear" :class="{'text-sm text-sky-300/30': inProcess || activeYear >= 2021}"
-           class="select-none cursor-pointer text-sky-300 ml-4 pi pi-caret-right h-fit"></i>
+      <div class="text-gray-300 w-full max-w-[980px] bg-sky-950/75 px-6 py-4 rounded-lg font-sans mt-6 lg:mt-12">
+        <span class="font-bold text-sky-500">Наша цель</span> – оказание полного комплекса консультационных услуг в области оценки и консалтинга (финансового,
+        управленческого, налогового и строительного) специалистами, обладающими международными квалификациями и
+        значительным опытом выполнения подобных проектов как в России, так и за рубежом.
       </div>
 
-      <div class="flex flex-col gap-y-2">
-        <div
-            v-for="rating of currentRating"
-            class="py-1.5 px-4 lg:py-1.5 lg:px-2.5 bg-sky-950 gap-y-1 rounded-xl lg:rounded-full flex flex-col lg:flex-row lg:items-center lg:justify-end"
-        >
-          <div class="text-sm text-gray-300 font-medium text-center lg:text-right">{{ rating.title }}</div>
+      <div class="text-gray-300 w-full max-w-[980px] bg-sky-950/75 px-6 py-4 rounded-lg font-sans">
+        <span class="font-bold text-emerald-500">Наши специалисты</span>
+        успешно осуществили и завершили проекты для многих (более 50) компаний из рейтинга «Крупнейшие
+        компании России» (рейтинг Эксперт-400).
+      </div>
+
+      <div class="z-10 flex flex-col gap-y-4 rounded-xl mt-2">
+
+        <div class="px-6 flex justify-center items-center gap-x-8">
+          <i @click="prevYear" :class="{'text-sm text-sky-500/40': inProcess || activeYear <= 2011}"
+             class="select-none cursor-pointer text-sky-500 ml-4 pi pi-caret-left h-fit"></i>
+          <div class="text-sky-500 font-bold">{{ activeYear }} г</div>
+          <i @click="nextYear" :class="{'text-sm text-sky-500/40': inProcess || activeYear >= 2021}"
+             class="select-none cursor-pointer text-sky-500 ml-4 pi pi-caret-right h-fit"></i>
+        </div>
+
+        <div class="flex flex-col gap-y-2">
           <div
-              class="relative ml-2 md:min-w-[240px] lg:max-w-[360px] bg-sky-900 rounded-md lg:rounded-full overflow-hidden">
-            <div class="w-full h-fit bg-gradient-to-r from-blue-800 to-sky-700 rounded-md lg:rounded-full"
-                 :style="{width: rating.value * 2 + '%'}">
-              <div class="opacity-0 text-xs">-1</div>
-            </div>
-            <div class="absolute right-0 top-0 mt-[1px] mr-3 text-xs text-sky-300 font-medium">
-              {{ rating.value }} шт
+              v-for="rating of currentRating"
+              class=" py-1.5 px-4 lg:py-1.5 lg:px-2.5 bg-sky-950/75 gap-y-1 rounded-xl lg:rounded-full flex flex-col lg:flex-row lg:items-center lg:justify-end"
+          >
+            <div class="text-sm text-gray-300 font-medium text-center lg:text-right font-sans">{{ rating.title }}</div>
+            <div
+                class="relative ml-2 md:min-w-[240px] lg:max-w-[360px] bg-sky-900 rounded-md lg:rounded-full overflow-hidden">
+              <div class="w-full h-fit bg-gradient-to-r from-blue-800 to-sky-700 rounded-md lg:rounded-full"
+                   :style="{width: rating.value * 2 + '%'}">
+                <div class="opacity-0 text-xs">-1</div>
+              </div>
+              <div class="absolute right-0 top-0 mt-[1px] mr-3 text-xs text-sky-300 font-medium">
+                {{ rating.value }}
+              </div>
             </div>
           </div>
         </div>
+
       </div>
 
+      <div class="flex flex-wrap justify-center gap-2 md:gap-4 mt-4">
+        <div class="w-full sm:w-[30%] text-gray-300 w-full bg-sky-950/75 px-6 py-4 rounded-lg font-sans">
+          <i class="text-xl select-none cursor-pointer text-sky-500 ml-8 mr-2 pi pi-info-circle h-fit"></i>
+          Детальное понимание требований к оценочным и консалтинговым продуктам со стороны аудиторских компаний,
+          коммерческих и инвестиционных банков, страховых компаний.
+        </div>
+        <div class="w-full sm:w-[30%] text-gray-300 w-full bg-sky-950/75 px-6 py-4 rounded-lg font-sans">
+          <i class="text-xl select-none cursor-pointer text-sky-500 ml-8 mr-2 pi pi-pencil h-fit"></i>
+          Эффективное планирование задач в рамках проектов благодаря значительному опыту сотрудников команды.
+        </div>
+        <div class="w-full sm:w-[30%] text-gray-300 w-full bg-sky-950/75 px-6 py-4 rounded-lg font-sans">
+          <i class="text-xl select-none cursor-pointer text-sky-500 ml-8 mr-2 pi pi-database h-fit"></i>
+          Возможность выполнения проектов в сжатые сроки благодаря существующим наработкам и базе знаний.
+        </div>
+        <div class="w-full sm:w-[30%] text-gray-300 w-full bg-sky-950/75 px-6 py-4 rounded-lg font-sans">
+          <i class="text-xl select-none cursor-pointer text-sky-500 ml-8 mr-2 pi pi-check-circle h-fit"></i>
+          Оптимальное соотношение «цена-качество».
+        </div>
+        <div class="w-full sm:w-[30%] text-gray-300 w-full bg-sky-950/75 px-6 py-4 rounded-lg font-sans">
+          <i class="text-xl select-none cursor-pointer text-sky-500 ml-8 mr-2 pi pi-users h-fit"></i>
+          Возможность привлечения к выполнению проектов специалистов, обладающих международными квалификациями и опытом в
+          различных областях знаний.
+        </div>
+      </div>
     </div>
-
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">
-      Наша цель – оказание полного комплекса консультационных услуг в области оценки и консалтинга (финансового,
-      управленческого, налогового и строительного) специалистами, обладающими международными квалификациями и
-      значительным опытом выполнения подобных проектов как в России, так и за рубежом.
-    </div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">
-      Наши специалисты успешно осуществили и завершили проекты для многих (более 50) компаний из рейтинга «Крупнейшие
-      компании России» (рейтинг Эксперт-400).
-    </div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">
-      Детальное понимание требований к оценочным и консалтинговым продуктам со стороны аудиторских компаний,
-      коммерческих и инвестиционных банков, страховых компаний.
-    </div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">
-      Эффективное планирование задач в рамках проектов благодаря значительному опыту сотрудников команды.
-    </div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">
-      Возможность выполнения проектов в сжатые сроки благодаря существующим наработкам и базе знаний.
-    </div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">
-      Оптимальное соотношение «цена-качество».
-    </div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">
-      Возможность привлечения к выполнению проектов специалистов, обладающих международными квалификациями и опытом в
-      различных областях знаний.
-    </div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">...</div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">...</div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">...</div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">...</div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">...</div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">...</div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">...</div>
-    <div class="w-full max-w-[980px] min-h-[120px] bg-gray-900/15 px-6 py-4 rounde-lg">...</div>
+    <evFooter/>
   </div>
 </template>
 
@@ -83,6 +78,7 @@
 import {ratingConfig} from "~/configs/ratingConfig.ts"
 import 'vue3-carousel/dist/carousel.css'
 import {Carousel, Slide, Pagination, Navigation} from 'vue3-carousel'
+import evFooter from "../components/widgets/ev-footer";
 
 export default {
   name: "rating",
@@ -106,7 +102,8 @@ export default {
   },
 
   components: {
-    Carousel, Slide, Pagination, Navigation
+    Carousel, Slide, Pagination, Navigation,
+    evFooter,
   },
 
   data() {
@@ -192,11 +189,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
 .rating-container {
-  @apply overflow-y-scroll;
-  @apply flex flex-col items-center gap-y-4;
-  @apply bg-cyan-900/60 gap-x-4 min-h-[100vh] max-h-[100vh];
-  @apply px-2 lg:px-16;
+  @apply flex flex-col items-center gap-y-2 md:gap-y-4;
+  @apply bg-cyan-900/60 gap-x-4 min-h-[100vh];
+  @apply px-3 lg:px-16;
   @apply pt-24 pb-12 sm:pt-28 lg:pt-32;
 
   &::before {
@@ -216,4 +213,5 @@ export default {
     visibility: hidden;
   }
 }
+
 </style>
