68cfa5a5d10231d37d05191f5b5d948f084f1d11
Anton Keller <ToniWebDev@mail.ru>
2025-01-28 17:15:38 +0300

    add news

--- components/ratingBlock.vue
+++ components/ratingBlock.vue
@@ -1,24 +1,15 @@
 <template>
   <div class="square relative w-full bg-gray-100 flex flex-col items-center py-2 md:py-6 lg:py-8 overflow-hidden">
 
-    <div class="absolute left-0 bottom-0 right-0 top-0 bg-flashes opacity-70"></div>
-
-
-    <!--    <div class="z-10 absolute bottom-[-25vw] md:bottom-[-20vw] opacity-15 sm:opacity-15 w-[100vw] md:w-[90vw]">-->
-    <!--      <svg viewBox="0 0 1024 1024" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg">-->
-    <!--        <path-->
-    <!--            d="M403.2 776l-62.4 62.4c-1.6 1.6-3.2 1.6-6.4 1.6h-88c-4.8 0-8-3.2-8-8s3.2-8 8-8h84.8l59.2-59.2v-68.8c0-4.8 3.2-8 8-8s8 3.2 8 8v64H576c4.8 0 8 3.2 8 8s-3.2 8-8 8H403.2z m-11.2-436.8l-108.8-94.4c-3.2-3.2-3.2-8-1.6-11.2 3.2-3.2 8-3.2 11.2-1.6l110.4 94.4H528c4.8 0 8 3.2 8 8s-3.2 8-8 8h-120V400c0 4.8-3.2 8-8 8s-8-3.2-8-8v-60.8zM800 728c-4.8 0-8-3.2-8-8s3.2-8 8-8h88c4.8 0 8 3.2 8 8s-3.2 8-8 8H800z m-49.6-435.2c-3.2 3.2-8 3.2-11.2 1.6-3.2-3.2-3.2-8-1.6-11.2l96-112c3.2-3.2 8-3.2 11.2-1.6 3.2 3.2 3.2 8 1.6 11.2l-96 112zM160 504c-4.8 0-8-3.2-8-8s3.2-8 8-8h112c4.8 0 8 3.2 8 8s-3.2 8-8 8h-112z m536 144c0 4.8-3.2 8-8 8s-8-3.2-8-8V544c0-4.8 3.2-8 8-8s8 3.2 8 8v104z"-->
-    <!--            fill="rgba(109, 40, 217, .75)"/>-->
-    <!--      </svg>-->
-    <!--    </div>-->
+    <div class="absolute left-0 bottom-0 right-0 top-0 bg-flashes"></div>
 
     <div class="z-10 text-gray-950 w-full rounded-sm py-4 pb-4 px-4 max-w-[920px]">
       <div
           v-motion
-          :initial="{ opacity: 0, translateY: -30}"
-          :visible="{ opacity: 1, translateY: 0}"
-          :duration="1200"
-          :delay="300"
+          :initial="{ opacity: 0, translateX: -30}"
+          :visible="{ opacity: 1, translateX: 0}"
+          :duration="600"
+          :delay="400"
           class="flex items-center text-lg md:text-2xl lg:text-4xl font-bold rounded-sm text-gray-800/95 w-fit">
         Рейтинг нашей компании
       </div>
@@ -27,10 +18,10 @@
 
         <div
             v-motion
-            :initial="{ opacity: 0, translateY: -30}"
-            :visible="{ opacity: 1, translateY: 0}"
-            :duration="1200"
-            :delay="600"
+            :initial="{ opacity: 0, scale: 0.95 }"
+            :visible="{ opacity: 1, scale: 1 }"
+            :duration="500"
+            :delay="900"
         >
           Наши специалисты успешно осуществили и завершили проекты для многих (более 50) компаний из рейтинга
           «Крупнейшие
@@ -101,8 +92,9 @@
       <div class="flex flex-wrap justify-between items-start w-full bg-transparent mt-4 md:mt-8 lg:mt-12">
         <div
             v-motion
-            :initial="{ opacity: 0, scale: 0.85 }"
-            :visible="{ opacity: 1, y: 0, scale: 1 }"
+            :initial="{ opacity: 0, scale: 0.9 }"
+            :visible="{ opacity: 1, scale: 1 }"
+            :duration="400"
             :delay="400"
             v-for="e of titles"
             :key="e.id"
